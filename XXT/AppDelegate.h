0a492bff9251317e98e12135eeafc4d9a96e3a63
hua <872987720@qq.com>
2013-10-22 10:10:20 +0800

    Initial Commit

---
+++ XXT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XXT
+//
+//  Created by shorigo on 13-10-22.
+//  Copyright (c) 2013年 shori. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
